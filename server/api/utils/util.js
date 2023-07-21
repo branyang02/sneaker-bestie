f4225852443a0bd478eaa76d6276dac0b8f373d9
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-07-21 08:31:39 -0400

    put throw error to util file

---
+++ server/api/utils/util.js
@@ -0,0 +1,13 @@
+
+function throwError(res) {
+    return (err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    };
+  }
+
+module.exports = {
+  throwError,
+};
