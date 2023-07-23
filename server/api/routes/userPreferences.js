1cfd8400a2aea928d28c049122dd21228f3c8273
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-07-23 13:31:20 -0400

    added update preference for userPreferences

--- server/api/routes/userPreferences.js
+++ server/api/routes/userPreferences.js
@@ -17,7 +17,7 @@ const checkAuth = require("../middleware/check-auth");
 */
 
 // ADD USER PREFERENCE TO DATABASE
-router.post("/add-user-preference", checkAuth, (req, res, next) => {
+router.post("/add-preferences", checkAuth, (req, res, next) => {
   // Obtain fields from request body
   const { PreferredColor, PreferredBrand, PreferredType, PreferredPriceRange } =
     req.body;
@@ -48,8 +48,8 @@ router.post("/add-user-preference", checkAuth, (req, res, next) => {
 });
 
 // VIEW USER PREFERENCE FROM DATABASE
-router.get("/view-user-preference/:userID", checkAuth, (req, res, next) => {
-  const userID = req.params.userID;
+router.get("/view-preferences", checkAuth, (req, res, next) => {
+  const userID = req.user.userId;
   UserPreference.findOne({ userID: userID })
     .exec()
     .then((doc) => {
@@ -57,9 +57,45 @@ router.get("/view-user-preference/:userID", checkAuth, (req, res, next) => {
         res.status(200).json(doc);
       } else {
         res.status(404).json({
-          message: "No valid entry found for provided userID",
+          message: "No valid entry found for current User",
         });
       }
+    })
+    .catch(utilFunctions.throwError(res));
+});
+
+// UPDATE USER PREFERENCE IN DATABASE
+router.patch("/update-preferences", checkAuth, (req, res, next) => {
+  /*
+    example request body:
+      [
+        {
+          "propName": "PreferredColor",
+          "value": ["blue", "red", "green"]
+        }, {
+          "propName": "PreferredBrand",
+          "value": ["Nike", "Adidas", "Puma"]
+        }, {
+          "propName": "PreferredType",
+          "value": ["sneakers", "hoodies", "jackets"]
+        }, {
+          "propName": "PreferredPriceRange",
+          "value": [200, 300]
+        }
+      ]
+  */
+  const userID = req.user.userId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  UserPreference.updateOne({ userID: userID }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "User preferences updated",
+        updatedUserPreference: result,
+      });
     })
     .catch(utilFunctions.throwError(res));
 });
