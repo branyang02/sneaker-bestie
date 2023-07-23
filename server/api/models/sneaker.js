64785fe6133aad957fa4c108939da92d943299cf
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-07-22 22:15:48 -0400

    created sneakers model.

--- server/api/models/sneaker.js
+++ server/api/models/sneaker.js
@@ -1,17 +1,61 @@
 const mongoose = require("mongoose");
 
 // create a Schema
-const userSchema = mongoose.Schema({
+const sneakerSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  email: {
+  productId: {
     type: String,
     required: true,
     unique: true,
-    match:
-      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
   },
-  password: { type: String, required: true },
+  urlKey: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  styleId: {
+    type: String,
+    required: true,
+  },
+  productType: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  productAttributes: {
+    gender: {
+      type: String,
+      required: true,
+    },
+    season: {
+      type: String,
+      required: true,
+    },
+    releaseDate: {
+      type: Date,
+      required: true,
+    },
+    retailPrice: {
+      type: Number,
+      required: true,
+    },
+    colorway: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+  },
 });
 
 // convert Schema to a Model
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Sneaker", sneakerSchema);
