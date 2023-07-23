c9927332e22d9f628017d3dd78dedb272a6e481a
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-07-23 14:10:14 -0400

    update user preference and created view histories

---
+++ server/api/models/view_history.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+// create a Schema
+const viewHistorySchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+  viewedSneakers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Sneaker", required: true }],
+});
+
+// convert Schema to a Model
+module.exports = mongoose.model("ViewHistory", viewHistorySchema);
--- server/api/routes/viewHistories.js
+++ server/api/routes/viewHistories.js
@@ -4,21 +4,12 @@ const mongoose = require("mongoose");
 const utilFunctions = require("../utils/util");
 const fetch = require("node-fetch");
 
-const UserPreference = require("../models/user_preference");
+const viewHistory = require("../models/view_history");
 const checkAuth = require("../middleware/check-auth");
 
 // ADD USER PREFERENCE TO DATABASE
 router.post("/add-preferences", checkAuth, (req, res, next) => {
   // Obtain fields from request body
-  /*
-    example request body:
-      {
-        "PreferredColor": ["blue", "red"],
-        "PreferredBrand": ["Nike", "Adidas"],
-        "PreferredType": ["sneakers", "hoodies"],
-        "PreferredPriceRange": [100, 200]
-      }
-  */
   const { PreferredColor, PreferredBrand, PreferredType, PreferredPriceRange } =
     req.body;
 
