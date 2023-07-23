a4f9058a78bb89ab283e010f38ff4544ac00a8ba
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-07-23 12:21:04 -0400

    restructured check-auth

---
+++ server/api/middleware/check-auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers["authorization"]; // get token from header
+  const token = authHeader && authHeader.split(" ")[1]; // split token from "Bearer <token>"
+  if (token == null) {
+    return res.sendStatus(401); // if there isn't any token
+  }
+
+  jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+};
