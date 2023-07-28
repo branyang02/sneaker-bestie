b7d643bfdc870146befecd7f2e62c627756c8088
Brandon Yang <107154811+branyang02@users.noreply.github.com>
2023-07-27 20:14:39 -0400

    configured left pane pannel with button

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/Home.css";
 import SlideShow from "./SlideShow";
+import LeftPane from "./LeftPane";
 
 function Home() {
+  const [isPaneOpen, setIsPaneOpen] = useState(false);
+
   return (
     <>
       <div className="home-container">
         <h1>Welcome to our website</h1>
         <p>Navigate to Signup or Sneakers List from the navigation above</p>
+        <button onClick={() => setIsPaneOpen(true)}>Open Pane</button>
       </div>
       <div className="slideshow-container">
         <SlideShow />
+      </div>
+      <div className="left-pane-button">
+        <LeftPane isOpen={isPaneOpen} onClose={() => setIsPaneOpen(false)} />
       </div>
     </>
   );
---
+++ client/src/components/LeftPane.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SlidingPane from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
+
+const LeftPane = ({ isOpen, onClose }) => {
+  return (
+    <div>
+      <SlidingPane
+        closeIcon={<div>Some div containing custom close icon.</div>}
+        isOpen={isOpen}
+        title="Hey, it is optional pane title.  I can be React component too."
+        from="left"
+        width="200px"
+        onRequestClose={onClose}
+      >
+        <div>And I am pane content on left.</div>
+      </SlidingPane>
+    </div>
+  );
+};
+
+export default LeftPane;
